3020c1a198f1232afc46bd2f2758213494b36c89
yudityudiarto <yudit.yudiarto@tokopedia.com>
2021-10-08 16:39:40 +0700

    enc dec

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
+	"github.com/ariefmaulidy/security-workshop/encryption"
 	"github.com/ariefmaulidy/security-workshop/messaging"
 	"github.com/nsqio/go-nsq"
 )
@@ -53,8 +55,8 @@ func main() {
 
 	// initiate consumer
 	consumer, err := messaging.NewConsumer(messaging.ConsumerConfig{
-		Topic:         "test", // Change the topic
-		Channel:       "test", // Change the channel
+		Topic:         "security_workshop_topic",   // Change the topic
+		Channel:       "security_workshop_channel", // Change the channel
 		LookupAddress: "172.18.59.254:4161",
 		MaxAttempts:   defaultConsumerMaxAttempts,
 		MaxInFlight:   defaultConsumerMaxInFlight,
@@ -72,13 +74,63 @@ func main() {
 
 func handlePublish(w http.ResponseWriter, r *http.Request) {
 	// Do Publish
-	topic := "" // TODO: update to given topic name
-	msg := ""   // TODO: write your message here
+
+	payment := &Payment{
+		UserData: User{
+			Name:        "yudit",
+			Email:       "yudit@email.com",
+			PhoneNumber: "01234567890",
+			Age:         21,
+		},
+		ItemData: []Item{
+			{
+				Name:       "item1",
+				Price:      12000,
+				Category:   "jeruk",
+				Quantity:   1,
+				TotalPrice: 12000,
+			},
+			{
+				Name:       "item2",
+				Price:      12000,
+				Category:   "mangga",
+				Quantity:   1,
+				TotalPrice: 12000,
+			},
+		},
+		TotalPayment: 24000,
+	}
+
+	paymentByte, err := json.Marshal(&payment)
+	if err != nil {
+		panic(err)
+	}
+
+	encryptedPayment, err := encryption.EncryptAES("mykey", paymentByte)
+	if err != nil {
+		panic(err)
+	}
+
+	topic := "security_workshop_topic" // TODO: update to given topic name
+	msg := encryptedPayment            // TODO: write your message here
+
 	producer.Publish(topic, msg)
 }
 
 func handleMessage(message *nsq.Message) error {
 	// Handle message NSQ here
+
+	decryptedPayment, err := encryption.DecryptAES("mykey", string(message.Body))
+	if err != nil {
+		panic(err)
+	}
+
+	var payment Payment
+
+	err = json.Unmarshal(decryptedPayment, &payment)
+	if err != nil {
+		panic(err)
+	}
 
 	message.Finish()
 	return nil
